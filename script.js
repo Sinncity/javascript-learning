281319a42894f5ae92cae545eaec64854bfd5473
Tparker85 <pawka8855@gmail.com>
2022-04-05 15:46:25 -0600

    learning dom manipulation

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict' ;
+console.log (document.querySelector(`.message`).textContent);
+
+document.querySelector(`.message`).textContent = `you got it nigga🎆🎊🎉`
+
+document.querySelector(`.number`).textContent = 11;
+document.querySelector(`.score`).textContent = 10;
+
+document.querySelector(`.guess`) .value = 23;
+console.log(document.querySelector(`.guess`) .value);
+
+
